92864c4693772607ba5e693ab230fb26a4e39cbe
MiguelBits <mcv.cooper@gmail.com>
2024-11-13 11:18:14 +0700

    two bad working select token buttons :)

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,66 +1,83 @@
 "use client";
 
-import Link from "next/link";
+import { useState } from "react";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { Address } from "~~/components/scaffold-eth";
 
 const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
+  const [selectedTokenIn, setSelectedTokenIn] = useState("Select Token");
+  const [selectedTokenOut, setSelectedTokenOut] = useState("Select Token");
+  const [isDropdownInOpen, setIsDropdownInOpen] = useState(false);
+  const [isDropdownOutOpen, setIsDropdownOutOpen] = useState(false);
+
+  const handleTokenInSelect = (token: string) => {
+    setSelectedTokenIn(token);
+    setIsDropdownInOpen(false);
+  };
+
+  const handleTokenOutSelect = (token: string) => {
+    setSelectedTokenOut(token);
+    setIsDropdownOutOpen(false);
+  };
 
   return (
     <>
-      <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="px-5">
-          <h1 className="text-center">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
-          </h1>
-          <div className="flex justify-center items-center space-x-2 flex-col sm:flex-row">
-            <p className="my-2 font-medium">Connected Address:</p>
-            <Address address={connectedAddress} />
-          </div>
-
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/app/page.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
-        </div>
-
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contracts
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
+      <div>
+        <div className="hero bg-base-200 min-h-screen">
+          <div className="hero-content flex-col justify-center items-center">
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+              <form className="card-body">
+                <div className="form-control">
+                  <div className="dropdown">
+                    <div
+                      tabIndex={0}
+                      role="button"
+                      className="btn m-1"
+                      onClick={() => setIsDropdownInOpen(!isDropdownInOpen)}
+                    >
+                      {selectedTokenIn}
+                    </div>
+                    {isDropdownInOpen && (
+                      <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                        <li>
+                          <a onClick={() => handleTokenInSelect("USDT")}>USDT</a>
+                        </li>
+                        <li>
+                          <a onClick={() => handleTokenInSelect("USDC")}>USDC</a>
+                        </li>
+                      </ul>
+                    )}
+                  </div>
+                  <input type="number" placeholder="amount in" className="input input-bordered" required />
+                </div>
+                <div className="form-control">
+                  <div className="dropdown">
+                    <div
+                      tabIndex={0}
+                      role="button"
+                      className="btn m-1"
+                      onClick={() => setIsDropdownOutOpen(!isDropdownOutOpen)}
+                    >
+                      {selectedTokenOut}
+                    </div>
+                    {isDropdownOutOpen && (
+                      <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                        <li>
+                          <a onClick={() => handleTokenOutSelect("USDT")}>USDT</a>
+                        </li>
+                        <li>
+                          <a onClick={() => handleTokenOutSelect("USDC")}>USDC</a>
+                        </li>
+                      </ul>
+                    )}
+                  </div>
+                  <input type="number" placeholder="amount out" className="input input-bordered" required />
+                </div>
+                <div className="form-control mt-6">
+                  <button className="btn btn-primary">Login</button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
